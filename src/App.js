44283fd36d1be2eada402ddf895bd5cab236fed1
mayeoms <mayeoms.ss@gmail.com>
2024-09-30 20:20:55 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
 
-function App() {
+export default function App() {
+  const value = 0.5;
   return (
     <div className="App">
       <header className="App-header">
@@ -11,15 +15,23 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <Box sx={{ width: 200 }}>
+      <Stack spacing={2} direction="row" sx={{ alignItems: 'center', mb: 1 }}>
+
+        <Slider aria-label="Volume" value={value} />
+
+      </Stack>
+      <Slider disabled defaultValue={30} aria-label="Disabled slider" />
+    </Box>
       </header>
+
     </div>
   );
 }
 
-export default App;
